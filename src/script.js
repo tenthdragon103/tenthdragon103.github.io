d1b3b9e131265c09cce84360c402393417fe30c3
tenthdragon103 <115429203+tenthdragon103@users.noreply.github.com>
2025-03-18 23:00:50 -0700

    base page

---
+++ src/script.js
@@ -0,0 +1,20 @@
+function getUsrInfo() {
+    //whoami
+    //create thingy that goes down and says:
+    //Your Information
+    //email
+    //user ID
+    //ip
+    //logout
+    document.getElementById("userdropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.usrNameButton')) {
+        var myDropdown = document.getElementById("userdropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
